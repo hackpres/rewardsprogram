049926b5eabdbb775b12487b257cb55a9092f53c
hackpres <102390515+hackpres@users.noreply.github.com>
2022-11-30 16:04:02 -0500

    added a console.log for graphql url

--- server/index.js
+++ server/index.js
@@ -76,6 +76,9 @@ const startApolloServer = async (typeDefs, resolvers) => {
     db.once('open', () => {
         app.listen(PORT, () => {
             console.log(`Server running on ${PORT}`);
+            console.log(
+                `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+              );
         });
         // const { url } = await startStandaloneServer(server, {
         //     listen: { port: 3001 },
