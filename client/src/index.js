97b2cbf03fb90c86937117729df9af534db2c9e1
hackpres <102390515+hackpres@users.noreply.github.com>
2022-11-30 16:28:00 -0500

    added setContext to client index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
 import './index.css';
 import App from './App';
 
@@ -8,8 +9,16 @@ import App from './App';
 const httpLink = createHttpLink({
   uri: '/graphql'
 });
+const authLink = setContext((_, { headers }) => {
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers
+    },
+  };
+});
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 })
 
