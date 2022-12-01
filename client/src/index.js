ef86ad90771ac6b67a40052545c6dd150fa6a04b
hackpres <102390515+hackpres@users.noreply.github.com>
2022-12-01 16:32:05 -0500

    just trying things at this point

--- client/src/index.js
+++ client/src/index.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from "@apollo/client";
-import { setContext } from '@apollo/client/link/context';
+// import { setContext } from '@apollo/client/link/context';
 import './index.css';
 import App from './App';
 
 
-const httpLink = createHttpLink({
-  uri: '/graphql'
-});
-const authLink = setContext((_, { headers }) => {
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers
-    },
-  };
-});
+// const httpLink = createHttpLink({
+//   uri: '/graphql'
+// });
+// const authLink = setContext((_, { headers }) => {
+//   // return the headers to the context so httpLink can read them
+//   return {
+//     headers: {
+//       ...headers
+//     },
+//   };
+// });
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  uri: '/graphql',
   cache: new InMemoryCache(),
 })
 
