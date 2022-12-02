7eb057a288c02fee0bc014d4b023e10a9d8e6d16
hackpres <102390515+hackpres@users.noreply.github.com>
2022-12-02 15:58:40 -0500

    shrugs

--- client/src/index.js
+++ client/src/index.js
@@ -18,7 +18,7 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: httpLink,
   cache: new InMemoryCache(),
 })
 
