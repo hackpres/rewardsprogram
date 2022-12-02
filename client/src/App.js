71e83e78d6ce0248a7d2ba787ce4f458ea741553
hackpres <102390515+hackpres@users.noreply.github.com>
2022-12-02 16:21:08 -0500

    ugh

--- client/src/App.js
+++ client/src/App.js
@@ -30,7 +30,7 @@ function App() {
   // }, [loading, error, data])
   // if (loading) return <div>Loading...</div>;
   let data;
-  fetchData = async () => {
+  const fetchData = async () => {
     const endpoint = 'https://rewardsprogram.vercel.app/graphql';
     const headers = {
       "content-type": "application/json",
